555567a692024c6359e504880da6bab1110cafe5
wanderhungerbuhler <whfdev@gmail.com>
2020-11-20 12:47:44 -0300

    Project Coritran

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,12 +28,48 @@ export default class MyDocument extends Document {
     }
   }
 
+  
   render() {
+    const pageTitle = "Coritran - Comércio e Serviços";
+    const description = "A Coritran conta com uma equipe multidisciplinar, qualificada para oferecer o serviço com eficácia e eficiência."
+    const pageImage = "/images/logo-web.png"
+
     return (
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <title>Coritran - Comércio e Serviços</title>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+
+          <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+          <meta name="MobileOptimized" content="320" />
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="theme-color" content="#001F51" />
+          <meta name="msapplication-TileColor" content="#001F51" />
+          <meta name="referrer" content="no-referrer-when-downgrade" />
+          <meta name="google" content="notranslate" />
+
+          <meta property="og:title" content={pageTitle} />
+          <meta property="og:description" content={description} />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={pageTitle} />
+          <meta property="og:image" content={pageImage} />
+          <meta property="og:image:secure_url" content={pageImage} />
+          <meta property="og:image:alt" content="Thumbnail" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:title" content={pageTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@rocketseat" />
+          <meta name="twitter:creator" content="@rocketseat" />
+          <meta name="twitter:image" content={pageImage} />
+          <meta name="twitter:image:src" content={pageImage} />
+          <meta name="twitter:image:alt" content="Thumbnail" />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="620" />
         </Head>
         <body>
           <Main />
